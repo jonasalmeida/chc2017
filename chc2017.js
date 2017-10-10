518c81acf44b8216423e313af3b7ae5ba60e42c6
Jonas Almeida <jalmeida@mathbiol.org>
2017-10-10 19:33:05 -0400

    automated app_id in response too local vs github hosting

--- chc2017.js
+++ chc2017.js
@@ -1,13 +1,31 @@
 console.log('loaded chc2017.js')
 
 function chc2017(){
+    localStorage.chc2017_client_id='b3f7136b-f9ca-4498-96e8-d09e15ce00b5'
+    localStorage.chc2017_app_id='6c9bee89-d529-4593-a156-7e36513a6042'
+    if(location.href.match("http://localhost:8000/chc2017")){
+        localStorage.chc2017_client_id='d737b157-faef-4bfc-b7fe-2fc17e2f2794'
+        localStorage.chc2017_app_id='9934f08b-760e-4256-b5d7-b10c7813ecec'
+    }
+    
     if(document.getElementById('chc2017Div')){ // call it by default only if from the default index.html
         console.log('root div found')
+        appLink.innerHTML='This patient facing App is registered at <a href="https://code.cerner.com/developer/smart-on-fhir/apps/'+localStorage.chc2017_app_id+'" target="_blank">https://code.cerner.com/developer/smart-on-fhir/apps/'+localStorage.chc2017_app_id+'</a>'
         callAuth.onclick=chc2017.auth
         if(location.search.length>0){
-            var h = '<h3 style="color:green">This is the response of the OAUTH2 call, check original window for the syntax of the URL call</h3>'
-            h += '<p style="color:blue">'+location.search+'</p>'
-            chc2017Div.innerHTML=h
+            if(location.search.match('code=')){
+                var h = '<h3 style="color:green">This is the response of the OAUTH2 call, check original window for the syntax of the URL call</h3>'
+                h += '<p style="color:blue">'+location.search+'</p>'
+                h += '<h4><button id="getAccessToken">Get Access token</button></h4>'
+                chc2017Div.innerHTML=h
+                getAccessToken.onclick=function(){
+                    
+                }
+
+            }else if(true){
+                debugger
+            }
+                
         }
     }
     
@@ -37,7 +55,7 @@ chc2017.auth=function(){ // oauth authentication
     // use https://authz-demo.sandboxcerner.com/client/demo to go through teh moves
 
     var state= Math.random().toString().slice(2)+Date.now()
-    var url='https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/protocols/oauth2/profiles/smart-v1/personas/patient/authorize?redirect_uri='+location.href+'&client_id=b3f7136b-f9ca-4498-96e8-d09e15ce00b5&state='+state+'&response_type=code&aud=https://fhir-myrecord.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca&scope=patient/Patient.read'
+    var url='https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/protocols/oauth2/profiles/smart-v1/personas/patient/authorize?redirect_uri='+location.href+'&client_id='+localStorage.chc2017_client_id+'&state='+state+'&response_type=code&aud=https://fhir-myrecord.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca&scope=patient/Patient.read'
 
     console.log([url])
 
